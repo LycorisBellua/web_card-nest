541d28bfacce23f5823c5e86a15b4870f2ec7e97
Hong-CHP <93changhong@gmail.com>
2026-03-04 14:50:08 +0100

    [Frontend] Input sanitation and validation: email, username and password (#24)

---
+++ frontend/src/components/style/SignForm.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 400px;
+  margin: auto;
+`;
+
+export const FormGroup = styled.div`
+  margin-bottom: 14px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  input {
+    height: 30px;
+  }
+  p {
+    font-size: 12px;
+    margin: 0;
+  }
+`;
+
+export const ErrorText = styled.div`
+  color: #ce4341;
+  font-size: 12px;
+  margin-bottom: 14px;
+  p {
+    margin: 0;
+  }
+`;
+
+export const ButtonSubmitWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 14px;
+`;
+
+export const SuccessMsg = styled.p`
+  text-align: center;
+  margin-top: 14px;
+`;
