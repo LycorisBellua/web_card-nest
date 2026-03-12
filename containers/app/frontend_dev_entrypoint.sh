fac5e82542fa4c8c3c9e725b0e237430adf6b518
joshw34 <joshwhitley@msn.com>
2026-03-12 22:12:07 +0100

    fixed frontend dev build order
    this ensures tha the frontend is built before the backend starts

--- containers/app/frontend_dev_entrypoint.sh
+++ containers/app/frontend_dev_entrypoint.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+cd /app/frontend
+npm install
+npm run build
 cd /app/backend
 npm install
 npx prisma migrate deploy
 npm start &
 cd /app/frontend
-npm install
 exec npm run dev -- --host 0.0.0.0
