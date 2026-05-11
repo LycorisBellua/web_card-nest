26777685ff29e1446de86a6aa2b4dbd9370548a9
Hong-CHP <93changhong@gmail.com>
2026-05-11 14:35:41 +0200

    [Frontend] Black Crown QOL (#71)

--- frontend/src/game/canvas/CanvasCard.ts
+++ frontend/src/game/canvas/CanvasCard.ts
@@ -1,69 +1,77 @@
-import { makeCardImg, makeBackImg, fillRoundRect } from "./cardTextures";
+import { makeCardImg, makeBackImg, fillRoundRect } from './cardTextures';
 
-let _backImg: HTMLCanvasElement | null = null
+let _backImg: HTMLCanvasElement | null = null;
 
-function getBackImg() { return _backImg??= makeBackImg()}
+function getBackImg() {
+  return (_backImg ??= makeBackImg());
+}
 
 export class CanvasCard {
-	id: string;
-	label: string;
-	flipped = false;
-	hover = false;
-	isWinner = false;
-	rotation = 0;
+  id: string;
+  label: string;
+  flipped = false;
+  hover = false;
+  isWinner = false;
+  rotation = 0;
 
-	x: number;
-	y: number;
-	tx: number;
-	ty: number;
+  x: number;
+  y: number;
+  tx: number;
+  ty: number;
 
-	readonly w = 80;
-	readonly h = 120;
+  readonly w = 80;
+  readonly h = 120;
 
-	private frontImg : HTMLCanvasElement;
+  private frontImg: HTMLCanvasElement;
 
-	constructor(id:string, label:string, x = 0, y = 0) {
-		this.id = id
-		this.label = label
-		this.x = this.tx = x
-		this.y = this.ty = y
-		this.frontImg = makeCardImg(label)
-	}
+  constructor(id: string, label: string, x = 0, y = 0) {
+    this.id = id;
+    this.label = label;
+    this.x = this.tx = x;
+    this.y = this.ty = y;
+    this.frontImg = makeCardImg(label);
+  }
 
-	// Xnext = (Xtarget - Xnow) * (1 - 0.001**dt)
-	update(dt: number) {
-		const k = 1 - Math.pow(0.001, dt)
-		this.x += (this.tx - this.x) * k
-		this.y += (this.ty - this.y) * k
-	}
+  // Xnext = (Xtarget - Xnow) * (1 - 0.001**dt)
+  update(dt: number) {
+    const k = 1 - Math.pow(0.001, dt);
+    this.x += (this.tx - this.x) * k;
+    this.y += (this.ty - this.y) * k;
+  }
 
-	draw(ctx: CanvasRenderingContext2D, isCurrent: boolean) {
-		const cx = this.x + this.w / 2
-    	const cy = this.y + this.h / 2
+  draw(ctx: CanvasRenderingContext2D, isCurrent: boolean) {
+    const cx = this.x + this.w / 2;
+    const cy = this.y + this.h / 2;
 
-    	ctx.save()
-    	ctx.translate(cx, cy)
-    	ctx.rotate(this.rotation)
-    	ctx.translate(-cx, -cy)
+    ctx.save();
+    ctx.translate(cx, cy);
+    ctx.rotate(this.rotation);
+    ctx.translate(-cx, -cy);
 
-		if (isCurrent) {
-			ctx.shadowColor = "rgba(255, 220, 40, 0.8)"
-			ctx.strokeStyle = "rgba(255, 220, 40, 0.6)"
-			ctx.shadowBlur = 30
-			ctx.lineWidth = 3
-			fillRoundRect(ctx, this.x, this.y, this.w + 1, this.h + 1, 6)
-			ctx.stroke()
-		} else if (this.isWinner) {
-			ctx.shadowColor = "rgb(194, 38, 255)"
-			ctx.strokeStyle = "rgb(192, 34, 255)"
-			ctx.shadowBlur = 30
-			ctx.lineWidth = 3
-			fillRoundRect(ctx, this.x, this.y, this.w + 1, this.h + 1, 6)
-			ctx.stroke()
-		}
-		fillRoundRect(ctx, this.x, this.y, this.w, this.h, 6)
-		ctx.clip()
-		ctx.drawImage(this.flipped? getBackImg() : this.frontImg, this.x, this.y, this.w, this.h)
-		ctx.restore()
-	}
+    if (isCurrent) {
+      ctx.shadowColor = 'rgba(255, 220, 40, 0.8)';
+      ctx.strokeStyle = 'rgba(255, 220, 40, 0.6)';
+      ctx.shadowBlur = 30;
+      ctx.lineWidth = 3;
+      fillRoundRect(ctx, this.x, this.y, this.w + 1, this.h + 1, 6);
+      ctx.stroke();
+    } else if (this.isWinner) {
+      ctx.shadowColor = 'rgb(194, 38, 255)';
+      ctx.strokeStyle = 'rgb(192, 34, 255)';
+      ctx.shadowBlur = 30;
+      ctx.lineWidth = 3;
+      fillRoundRect(ctx, this.x, this.y, this.w + 1, this.h + 1, 6);
+      ctx.stroke();
+    }
+    fillRoundRect(ctx, this.x, this.y, this.w, this.h, 6);
+    ctx.clip();
+    ctx.drawImage(
+      this.flipped ? getBackImg() : this.frontImg,
+      this.x,
+      this.y,
+      this.w,
+      this.h,
+    );
+    ctx.restore();
+  }
 }
--- frontend/src/game/state/initialState.ts
+++ frontend/src/game/state/initialState.ts
@@ -2,24 +2,27 @@ import type { Card, GameState } from '../logic/types';
 import { createDeck, shuffle } from 'game/logic/deck';
 
 // start a game
-export function initialGame(playerCount: number) : GameState {
-	const fullDeck: Card[] = createDeck()
-	const shuffledDeck: Card[] = shuffle(fullDeck)
-	return {
-		players: Array.from({length: playerCount}, (_, i)=>
-		({
-			id: i,
-			cards: [],
-			score: 0,
-			hasStood: false,
-			isBusted: false,
-			hasBlackCrown: false,
-			reachedAt: -2
-		})),
-		currentPlayerIdx: 0,
-		deck: shuffledDeck,
-		turn: 0,
-		gameStatus: "playing",
-		winnerId: null,
-	}
+export function initialGame(
+  playerCount: number,
+  currentUser?: { username: string },
+): GameState {
+  const fullDeck: Card[] = createDeck();
+  const shuffledDeck: Card[] = shuffle(fullDeck);
+  return {
+    players: Array.from({ length: playerCount }, (_, i) => ({
+      id: i,
+      username: i === 0 ? (currentUser?.username ?? null) : null,
+      cards: [],
+      score: 0,
+      hasStood: false,
+      isBusted: false,
+      hasBlackCrown: false,
+      reachedAt: -2,
+    })),
+    currentPlayerIdx: 0,
+    deck: shuffledDeck,
+    turn: 0,
+    gameStatus: 'playing',
+    winnerId: null,
+  };
 }
