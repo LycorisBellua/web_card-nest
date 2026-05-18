8a3efe175af30e0ce696135d0a84c696f3434205
Lycoris Bellua <lycoris.bellua@gmail.com>
2026-05-18 15:48:20 +0200

    Combine reset pwd pages into one

--- frontend/src/pages/auth/ResetPassword.tsx
+++ frontend/src/pages/auth/ResetPassword.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { sanitizeEmail } from 'functions/UserSanitation';
-import { validateEmail } from 'functions/UserValidation';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+import { sanitizeEmail, sanitizePassword } from 'functions/UserSanitation';
+import { validateEmail, validatePassword } from 'functions/UserValidation';
 import { ScrollablePage } from 'components/general/Scrollable';
 import { BtnDefault } from 'components/btn/Btn';
 import InputField from 'components/misc/InputField';
 
 function ResetPassword() {
+  const [searchParams] = useSearchParams();
+  const email = searchParams.get('email');
+  const token = searchParams.get('token');
+  if (email && token) {
+    return <ResetPasswordConfirm email={email} token={token} />;
+  }
+  return <ResetPasswordRequest />;
+}
+
+function ResetPasswordRequest() {
   const [email, setEmail] = useState('');
   const [errors, setErrors] = useState<string[]>([]);
   const [message, setMessage] = useState('');
@@ -69,7 +79,100 @@ function ResetPassword() {
           helpers={errors.length > 0 ? errors : [message]}
           isError={!!errors.length}
         />
-        <BtnDefault type="submit">Send code</BtnDefault>
+        <BtnDefault type="submit">Send link</BtnDefault>
+      </form>
+      <Link to="/auth">Go back to login page</Link>
+    </ScrollablePage>
+  );
+}
+
+function ResetPasswordConfirm({
+  email,
+  token,
+}: {
+  email: string;
+  token: string;
+}) {
+  const [message, setMessage] = useState('');
+  const [uPwd, setUPwd] = useState('');
+  const [uPwdConfirm, setUPwdConfirm] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
+  const navigate = useNavigate();
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setMessage('');
+    setErrors([]);
+    if (!uPwd || !uPwdConfirm) {
+      setErrors(['Please fill all fields']);
+      return;
+    }
+    if (uPwd !== uPwdConfirm) {
+      setErrors(["Passwords don't match"]);
+      return;
+    }
+    const userPwd = sanitizePassword(uPwd);
+    const allErrors = validatePassword(userPwd, '', email);
+    if (allErrors.length > 0) {
+      setErrors(allErrors);
+      return;
+    }
+    try {
+      const res = await fetch('/api/reset-password', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email,
+          token,
+          newPassword: userPwd,
+        }),
+      });
+      if (!res.ok) throw new Error('Password reset failed.');
+      setMessage('Password reset successfully!');
+      setUPwd('');
+      setUPwdConfirm('');
+      setErrors([]);
+      setTimeout(() => {
+        void navigate('/login');
+      }, 3000);
+    } catch {
+      setErrors(['Error occured']);
+    }
+  }
+
+  return (
+    <ScrollablePage>
+      <h1>Reset Password</h1>
+      <p>Email: {email.length ? email : '[Error]'}</p>
+      <form
+        onSubmit={(e) => {
+          void handleSubmit(e);
+        }}
+      >
+        <InputField
+          id="new-password"
+          type="password"
+          name="new-password"
+          label="New password"
+          autoComplete="new-password"
+          value={uPwd}
+          onChange={(e) => setUPwd(e.target.value)}
+          helpers={[
+            'You need a minimum of 8 characters, including one uppercase, one lowercase, one digit and one special character.',
+          ]}
+          isError={!!errors.length}
+        />
+        <InputField
+          id="confirm-password"
+          type="password"
+          name="confirm-password"
+          label="Confirm new password"
+          value={uPwdConfirm}
+          onChange={(e) => setUPwdConfirm(e.target.value)}
+          helpers={errors.length > 0 ? errors : [message]}
+          isError={!!errors.length}
+        />
+        <BtnDefault>Confirm</BtnDefault>
       </form>
       <Link to="/auth">Go back to login page</Link>
     </ScrollablePage>
